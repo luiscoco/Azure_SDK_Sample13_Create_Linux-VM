0b550294103f39466b1bcb4a6916d5b0433a0131
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-12-01 16:26:04 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Network.Models;
+using Azure.ResourceManager.Network;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Resources.Models;
+using Azure.ResourceManager.Compute;
+using Azure.ResourceManager.Compute.Models;
+
+
+ArmClient armClient = new ArmClient(new DefaultAzureCredential());
+SubscriptionResource subscription = await armClient.GetDefaultSubscriptionAsync();
+
+ResourceGroupCollection rgCollection = subscription.GetResourceGroups();
+// With the collection, we can create a new resource group with an specific name
+string rgName = "myRgName";
+AzureLocation location = AzureLocation.WestEurope;
+ResourceGroupResource resourceGroup = await rgCollection.CreateOrUpdate(WaitUntil.Started, rgName, new ResourceGroupData(location)).WaitForCompletionAsync();
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+PublicIPAddressCollection publicIPAddressCollection = resourceGroup.GetPublicIPAddresses();
+string publicIPAddressName = "20.61.0.157";
+PublicIPAddressData publicIPInput = new PublicIPAddressData()
+{
+    Location = resourceGroup.Data.Location,
+    PublicIPAllocationMethod = NetworkIPAllocationMethod.Dynamic,
+    DnsSettings = new PublicIPAddressDnsSettings()
+    {
+        DomainNameLabel = "mydomain12319741999"
+    }
+};
+PublicIPAddressResource publicIPAddress = await publicIPAddressCollection.CreateOrUpdate(WaitUntil.Completed, publicIPAddressName, publicIPInput).WaitForCompletionAsync();
+
+VirtualNetworkCollection virtualNetworkCollection = resourceGroup.GetVirtualNetworks();
+
+string vnetName = "myVnet";
+
+// Use the same location as the resource group
+VirtualNetworkData input = new VirtualNetworkData()
+{
+    Location = resourceGroup.Data.Location,
+    AddressPrefixes = { "10.0.0.0/16", },
+    DhcpOptionsDnsServers = { "10.1.1.1", "10.1.2.4" },
+    Subnets = { new SubnetData() { Name = "mySubnet", AddressPrefix = "10.0.1.0/24", } }
+};
+
+VirtualNetworkResource vnet = await virtualNetworkCollection.CreateOrUpdate(WaitUntil.Completed, vnetName, input).WaitForCompletionAsync();
+
+VirtualNetworkCollection virtualNetworkCollection1 = resourceGroup.GetVirtualNetworks();
+
+VirtualNetworkResource virtualNetwork1 = await virtualNetworkCollection1.GetAsync("myVnet");
+Console.WriteLine(virtualNetwork1.Data.Name);
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+NetworkInterfaceCollection networkInterfaceCollection = resourceGroup.GetNetworkInterfaces();
+string networkInterfaceName = "myNetworkInterface";
+NetworkInterfaceData networkInterfaceInput = new NetworkInterfaceData()
+{
+    Location = resourceGroup.Data.Location,
+    IPConfigurations = {
+        new NetworkInterfaceIPConfigurationData()
+        {
+            Name = "ipConfig",
+            PrivateIPAllocationMethod = NetworkIPAllocationMethod.Dynamic,
+            PublicIPAddress = new PublicIPAddressData()
+            {
+                Id = publicIPAddress.Id
+            },
+            Subnet = new SubnetData()
+            {
+                // use the virtual network just created
+                Id = virtualNetwork1.Data.Subnets[0].Id
+            }
+        }
+    }
+};
+
+// Create NSG rule for SSH
+NetworkSecurityGroupCollection nsgCollection = resourceGroup.GetNetworkSecurityGroups();
+string nsgName = "myNetworkSecurityGroup";
+NetworkSecurityGroupData nsgInput = new NetworkSecurityGroupData()
+{
+    Location = resourceGroup.Data.Location,
+    SecurityRules =
+            {
+                new SecurityRuleData()
+                {
+                    Name = "AllowSSH",
+                    Priority = 100,
+                    Access = SecurityRuleAccess.Allow,
+                    Direction = SecurityRuleDirection.Inbound,
+                    Protocol = SecurityRuleProtocol.Tcp,
+                    SourceAddressPrefix = "*",
+                    SourcePortRange = "*",
+                    DestinationAddressPrefix = "*",
+                    DestinationPortRange = "22", // SSH port
+                }
+            }
+};
+
+NetworkSecurityGroupResource nsg = await nsgCollection.CreateOrUpdate(WaitUntil.Completed, nsgName, nsgInput).WaitForCompletionAsync();
+
+// Associate NSG with the network interface
+networkInterfaceInput.NetworkSecurityGroup = new NetworkSecurityGroupData()
+{
+    Id = nsg.Id
+};
+
+NetworkInterfaceResource networkInterface = await networkInterfaceCollection.CreateOrUpdate(WaitUntil.Completed, networkInterfaceName, networkInterfaceInput).WaitForCompletionAsync();
+
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Now we get the virtual machine collection from the resource group
+VirtualMachineCollection vmCollection = resourceGroup.GetVirtualMachines();
+// Use the same location as the resource group
+string vmName = "myVM";
+string adminusername = "azureuser";
+VirtualMachineData input2 = new VirtualMachineData(resourceGroup.Data.Location)
+{
+    HardwareProfile = new VirtualMachineHardwareProfile()
+    {
+        VmSize = VirtualMachineSizeType.StandardE2SV3
+    },
+    OSProfile = new VirtualMachineOSProfile()
+    {
+        AdminUsername = adminusername,
+        ComputerName = "myVM",
+        LinuxConfiguration = new LinuxConfiguration()
+        {
+            DisablePasswordAuthentication = true,
+            SshPublicKeys = {
+                new SshPublicKeyConfiguration()
+                {
+                    Path = $"/home/" + adminusername + "/.ssh/authorized_keys",
+                    KeyData = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDU7y9f7dAjJxjESAKRU3HTXURWtFOkWhWMooT4GWkeghcgoJ2UiryLY9Pq7xQihh0c4/ar2rRRqgzHl/MLZicGgYxXMyS419H1JG1FYHSlsVqxXylvQMw/KlaL+DQFrwv9KOLpEHKF/WsQd8/8jWzy19xrNhHrQd/GE0DtEJ6TKb/2VUUGbWPE4tA85fdX7mu2dXxiAs18Gz2ANfgnipRjftBv9g89ISoJ7mGaLuIUEGesWIL3LV6uMuFVX0OXzXUHmVjtUXeUNCl/17GZtP0slnLFasOMyByrcAKw8sWBzNFyPNCisdpZhrJKLa6adxRDHKIELoGvfe9B9yhgKu59fk8i90tZPq00gWd83pulwrxzBFbVoVs7mdTsNaM66VS1MvIG6sQY+jRcd5RVdQUvTqLoGW+7FrLYgwIWxxefP7Js1ljGihTC7PY29AuQGokScMeFGmgQjWfPZAA3yWqBQbXdmI9qw269WSUNfMaSvkJ+MQxVAcy7a9apqD4Kr0E= generated-by-azure", //<value of the public ssh key>
+                }
+            }
+        }
+    },
+    NetworkProfile = new VirtualMachineNetworkProfile()
+    {
+        NetworkInterfaces =
+        {
+            new VirtualMachineNetworkInterfaceReference()
+            {
+                Id = new ResourceIdentifier("/subscriptions/846901e6-da09-45c8-98ca-7cca2353ff0e/resourceGroups/myRgName/providers/Microsoft.Network/networkInterfaces/" + networkInterfaceName),
+                Primary = true,
+            }
+        }
+    },
+    StorageProfile = new VirtualMachineStorageProfile()
+    {
+        OSDisk = new VirtualMachineOSDisk(DiskCreateOptionType.FromImage)
+        {
+            OSType = SupportedOperatingSystemType.Linux,
+            Caching = CachingType.ReadWrite,
+            ManagedDisk = new VirtualMachineManagedDisk()
+            {
+                StorageAccountType = StorageAccountType.StandardLrs
+            }
+        },
+        // ImageReference = new ImageReference()
+        // {
+        //     Publisher = "Canonical",
+        //     Offer = "UbuntuServer",
+        //     Sku = "18.04-LTS",
+        //     Version = "latest",
+        // },
+        // ImageReference = new ImageReference()
+        // {
+        //     Publisher = "Canonical",
+        //     Offer = "0001-com-ubuntu-server-focal",
+        //     Sku = "20_04-lts-gen2",
+        //     Version = "latest",
+        // },
+        // ImageReference = new ImageReference()
+        // {
+        //     Publisher = "RedHat",
+        //     Offer = "RHEL",
+        //     Sku = "87-gen2",
+        //     Version = "latest",
+        // },
+        ImageReference = new ImageReference()
+        {
+            Publisher = "Canonical",
+            Offer = "0001-com-ubuntu-server-jammy",
+            Sku = "22_04-lts-gen2",
+            Version = "latest",
+        }
+    }
+};
+ArmOperation<VirtualMachineResource> lro = await vmCollection.CreateOrUpdateAsync(WaitUntil.Completed, vmName, input2);
+VirtualMachineResource vm = lro.Value;
